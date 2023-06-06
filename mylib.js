ec7e541f3c71eebd10eb9d28f469c2bb63d2bea2
quan-ming <117795169+urusa11@users.noreply.github.com>
2023-06-07 01:00:49 +0800

    Update mylib.js

--- mylib.js
+++ mylib.js
@@ -27,7 +27,13 @@ fileUploadE2.addEventListener("change", function(e) {
   srcImgE2.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
-var s1, s1a, s2, c1, c2, srow = 0,scol = 0,crow = 0,ccol = 0,AC;
+var s1, s1a, s2, c1, c2, c3, c4, srow = 0,
+  scol = 0,
+  crow = 0,
+  ccol = 0,
+  c34row = 0,
+  c34col = 0,
+  A, B, A_inv;
 
 srcImgEl.onload = function() {
   var src = cv.imread(srcImgEl);
@@ -70,25 +76,33 @@ function getpixels(src) {
   return p;
 }
 
-function ab() {
-  this.a = Math.floor(Math.random() * 256);
-  while (this.a % 2 == 0)
-    this.a = Math.floor(Math.random() * 256);
-  this.b = Math.floor(1 + Math.random() * 255);
+function getAB() {
+  A = document.getElementById('AA').value * 1;
+  B = document.getElementById('AB').value * 1;
+  if (A % 2 == 0 || 256 % A == 0 || A < 0)
+    window.alert("錯誤:隨機數A錯誤");
+  if (B < 1 || B > 255)
+    window.alert("錯誤:隨機數B錯誤");
+  //console.log(A, B);
+}
+
+function getA_inv(n) {
+  var inv;
   for (var i = 0; i < 256; i++) {
-    var flag = (this.a * i) % 256;
+    var flag = (n * i) % 256;
     if (flag == 1)
-      this.a_inv = i;
+      inv = i;
   }
+  return inv;
 }
 
 function affine() {
+  getAB();
   AC = new getarr(srow);
   s1a = new getarr(srow);
   for (var y = 0; y < srow; y++) {
     for (var x = 0; x < scol; x++) {
-      AC[y][x] = new ab();
-      s1a[y][x] = new srgb((s1[y][x].r * AC[y][x].a + AC[y][x].b) % 256, (s1[y][x].g * AC[y][x].a + AC[y][x].b) % 256, (s1[y][x].b * AC[y][x].a + AC[y][x].b) % 256, 255);
+      s1a[y][x] = new srgb((s1[y][x].r * A + B) % 256, (s1[y][x].g * A + B) % 256, (s1[y][x].b * A + B) % 256, 255);
     }
   }
 }
@@ -162,7 +176,12 @@ function showphotos() {
             imgData.data[y * ccol * 4 + x * 4 + 0] = tmp[p - 1][y][x].r;
             imgData.data[y * ccol * 4 + x * 4 + 1] = tmp[p - 1][y][x].g;
             imgData.data[y * ccol * 4 + x * 4 + 2] = tmp[p - 1][y][x].b;
-            imgData.data[y * ccol * 4 + x * 4 + 3] = tmp[p - 1][y][x].s;
+            if (y == 0 && x == 0)
+              imgData.data[y * ccol * 4 + x * 4 + 3] = A;
+            else if (y == 0 && x == 1)
+              imgData.data[y * ccol * 4 + x * 4 + 3] = B;
+            else
+              imgData.data[y * ccol * 4 + x * 4 + 3] = tmp[p - 1][y][x].s;
           }
         }
         ctx.putImageData(imgData, 0, 0);
@@ -171,57 +190,118 @@ function showphotos() {
   }
 }
 
+var fileUploadE3 = document.getElementById('file-upload3'),
+  srcImgE3 = document.getElementById('src-image3')
+fileUploadE3.addEventListener("change", function(e) {
+  srcImgE3.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+
+var fileUploadE4 = document.getElementById('file-upload4'),
+  srcImgE4 = document.getElementById('src-image4')
+fileUploadE4.addEventListener("change", function(e) {
+  srcImgE4.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+
+srcImgE3.onload = function() {
+  var src = cv.imread(srcImgE3);
+  c34row = src.rows;
+  c34col = src.cols;
+  c3 = getpixels(src);
+}
+
+srcImgE4.onload = function() {
+  var src = cv.imread(srcImgE4);
+  c4 = getpixels(src);
+}
+
 function showphoto() {
-  var a = -1,b = -1;
-  var markedCheckbox = document.querySelectorAll('input[type="checkbox"]:checked');
-  for (var checkbox of markedCheckbox) {
-    if (a == -1) a = checkbox.value;
-    else b = checkbox.value;
-  }
-  var c = document.getElementById('canvas'),
-    ctx = c.getContext('2d'),
-    imgData = ctx.createImageData(srow, scol);
-  if (a != -1 && b != -1) {
-    document.getElementById("err").innerHTML = ('');
-    var c_a = document.getElementById('canvas' + a),
-      ctx_a = c_a.getContext('2d'),
-      imgData_a = ctx_a.getImageData(0, 0, ccol, crow),
-      c_b = document.getElementById('canvas' + b),
-      ctx_b = c_b.getContext('2d'),
-      imgData_b = ctx_b.getImageData(0, 0, ccol, crow),
-      tmp = [];
-    c.width = scol;
-    c.height = srow;
-    for (var i = 0; i < imgData_a.data.length; i++) {
-      if (i % 4 == 3) {
-        tmp[i] = 255;
-        continue;
+  if (c3 != null && c4 != null) {
+    var AA = c3[0][0].s,
+      BB = c3[0][1].s;
+    var AA_inv = getA_inv(AA);
+    var c = document.getElementById('canvas'),
+      ctx = c.getContext('2d'),
+      imgData = ctx.createImageData(c34row / 2, c34col / 2),
+      tmpc = [];
+    c.width = c34col / 2;
+    c.height = c34row / 2;
+    for (var y = 0; y < c34row; y++) {
+      for (var x = 0; x < c34col; x++) {
+        tmpc[y * 4 * c34col + x * 4 + 0] = c3[y][x].r ^ c4[y][x].r;
+        tmpc[y * 4 * c34col + x * 4 + 1] = c3[y][x].g ^ c4[y][x].g;
+        tmpc[y * 4 * c34col + x * 4 + 2] = c3[y][x].b ^ c4[y][x].b;
+        tmpc[y * 4 * c34col + x * 4 + 3] = 255;
       }
-      tmp[i] = imgData_a.data[i] ^ imgData_b.data[i];
     }
-    for (var y = 0; y < srow; y++) {
-      for (var x = 0; x < scol; x++) {
+    for (var y = 0; y < c34row / 2; y++) {
+      for (var x = 0; x < c34col / 2; x++) {
         for (var t = 0; t < 4; t++) {
-          var z = Math.max(tmp[2 * y * ccol * 4 + 2 * x * 4 + t], tmp[2 * y * ccol * 4 + (2 * x + 1) * 4 + t], tmp[(2 * y + 1) * ccol * 4 + 2 * x * 4 + t], tmp[(2 * y + 1) * ccol * 4 + (2 * x + 1) * 4 + t]) - AC[y][x].b;
+          var z = Math.max(tmpc[2 * y * c34col * 4 + 2 * x * 4 + t], tmpc[2 * y * c34col * 4 + (2 * x + 1) * 4 + t], tmpc[(2 * y + 1) * c34col * 4 + 2 * x * 4 + t], tmpc[(2 * y + 1) * c34col * 4 + (2 * x + 1) * 4 + t]) - BB;
           if (z < 0) z += 256;
-          imgData.data[y * scol * 4 + x * 4 + t] = z * AC[y][x].a_inv % 256;
+          imgData.data[y * c34col / 2 * 4 + x * 4 + t] = z * AA_inv % 256;
         }
       }
     }
     for (var i = 3; i < imgData.data.length; i += 4)
       imgData.data[i] = 255;
     ctx.putImageData(imgData, 0, 0);
-    document.getElementById("err").innerHTML = ('圖'+a+" XOR 圖"+b);
   } else {
-    window.alert('請選擇兩張圖片');
-    for (var i = 0; i < imgData.data.length; i++)
-      imgData.data[i] = 0;
-    ctx.putImageData(imgData, 0, 0);
+    var a = -1,
+      b = -1;
+    var markedCheckbox = document.querySelectorAll('input[type="checkbox"]:checked');
+    for (var checkbox of markedCheckbox) {
+      if (a == -1) a = checkbox.value;
+      else b = checkbox.value;
+    }
+    var c = document.getElementById('canvas'),
+      ctx = c.getContext('2d'),
+      imgData = ctx.createImageData(srow, scol);
+    if (a != -1 && b != -1) {
+      document.getElementById("err").innerHTML = ('');
+      var c_a = document.getElementById('canvas' + a),
+        ctx_a = c_a.getContext('2d'),
+        imgData_a = ctx_a.getImageData(0, 0, ccol, crow),
+        c_b = document.getElementById('canvas' + b),
+        ctx_b = c_b.getContext('2d'),
+        imgData_b = ctx_b.getImageData(0, 0, ccol, crow),
+        tmp = [];
+      c.width = scol;
+      c.height = srow;
+      for (var i = 0; i < imgData_a.data.length; i++) {
+        if (i % 4 == 3) {
+          tmp[i] = 255;
+          continue;
+        }
+        tmp[i] = imgData_a.data[i] ^ imgData_b.data[i];
+      }
+      var AA = imgData_a.data[3],
+        BB = imgData_a.data[7];
+      var AA_inv = getA_inv(AA);
+      //console.log(A, B, AA, BB, AA_inv);
+      for (var y = 0; y < srow; y++) {
+        for (var x = 0; x < scol; x++) {
+          for (var t = 0; t < 4; t++) {
+            var z = Math.max(tmp[2 * y * ccol * 4 + 2 * x * 4 + t], tmp[2 * y * ccol * 4 + (2 * x + 1) * 4 + t], tmp[(2 * y + 1) * ccol * 4 + 2 * x * 4 + t], tmp[(2 * y + 1) * ccol * 4 + (2 * x + 1) * 4 + t]) - BB;
+            if (z < 0) z += 256;
+            imgData.data[y * scol * 4 + x * 4 + t] = z * AA_inv % 256;
+          }
+        }
+      }
+      for (var i = 3; i < imgData.data.length; i += 4)
+        imgData.data[i] = 255;
+      ctx.putImageData(imgData, 0, 0);
+      document.getElementById("err").innerHTML = ('圖' + a + " XOR 圖" + b);
+    } else {
+      window.alert('請選擇兩張圖片');
+      for (var i = 0; i < imgData.data.length; i++)
+        imgData.data[i] = 0;
+      ctx.putImageData(imgData, 0, 0);
+    }
   }
 }
 
 function rechoose() {
-document.getElementById("err").innerHTML = ('');
+  document.getElementById("err").innerHTML = ('');
   $('input:checkbox').removeAttr('checked');
   $("input:checkbox").prop("disabled", false);
   var c = document.getElementById('canvas'),
@@ -244,6 +324,10 @@ function check() {
         n += 4;
     }
   }
+  console.log(s1[0][0].r,s1[0][0].g,s1[0][0].b,s1[0][0].s);
+  console.log(imgData.data[0],imgData.data[1],imgData.data[2],imgData.data[3]);
+  console.log(s1[0][1].r,s1[0][1].g,s1[0][1].b,s1[0][1].s);
+  console.log(imgData.data[4],imgData.data[5],imgData.data[6],imgData.data[7]);
   document.getElementById("check").innerHTML = ('與原圖' + n + '處相同,' + n / imgData.data.length * 100 + '%相符');
 }
 
